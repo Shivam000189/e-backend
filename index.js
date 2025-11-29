2a80093e7c403026248d484e8c75652093abc194
Shivam000189 <shivamsharmass9897@gmail.com>
2025-11-29 16:48:28 +0530

    Starting

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+const app = express();
+PORT = process.env.PORT || 3000;
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+
+
+
